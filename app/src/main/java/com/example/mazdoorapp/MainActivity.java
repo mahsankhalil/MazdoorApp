db99c7f3187cc43b3abe6fe7914c92e148b1ed98
Muhammad Ahsan Khalil <mahsankhalil@outlook.com>
2019-07-05 08:55:28 +0500

    completed

--- app/src/main/java/com/example/mazdoorapp/MainActivity.java
+++ app/src/main/java/com/example/mazdoorapp/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.mazdoorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://freelance.azurewebsites.net");
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
